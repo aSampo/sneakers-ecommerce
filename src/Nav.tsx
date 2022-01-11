c70e10d11a5ecb36503697d1af3a477e2a1b40fd
Andres Sampo <andressampo@gmail.com>
2022-01-11 16:21:41 -0300

    tabs

--- src/Nav.tsx
+++ src/Nav.tsx
@@ -8,24 +8,24 @@ interface Props { }
 
 const Nav: React.VFC<Props> = (props) => {
 
-    return (
-        <Stack paddingY={8} direction="row" justifyContent="space-between" alignItems="center">
-            <Stack direction="row" spacing={12} alignItems="center">
-                <Image src={logo} />
-                <Stack spacing={6} direction="row" color="gray.500" fontSize="sm">
-                    <Link>Collections</Link>
-                    <Link>Man</Link>
-                    <Link>Woman</Link>
-                    <Link>About</Link>
-                    <Link>Contact</Link>
-                </Stack>
-            </Stack>
-            <Stack direction="row" spacing={6} alignItems="center">
-                <Image src={cart} height={6} width={6}></Image>
-                <Avatar src={avatar} height={10} width={10}></Avatar>
-            </Stack>
+  return (
+    <Stack paddingY={8} direction="row" justifyContent="space-between" alignItems="center">
+      <Stack direction="row" spacing={12} alignItems="center">
+        <Image src={logo} />
+        <Stack spacing={6} direction="row" color="gray.500" fontSize="sm">
+          <Link>Collections</Link>
+          <Link>Man</Link>
+          <Link>Woman</Link>
+          <Link>About</Link>
+          <Link>Contact</Link>
         </Stack>
-    )
+      </Stack>
+      <Stack direction="row" spacing={6} alignItems="center">
+        <Image src={cart} height={6} width={6}></Image>
+        <Avatar src={avatar} height={10} width={10}></Avatar>
+      </Stack>
+    </Stack>
+  )
 }
 
 export default Nav
