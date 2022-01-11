3b1f2791cc763d2454ebdd9e60eb7d542afcf772
Andres Sampo <andressampo@gmail.com>
2022-01-11 16:37:28 -0300

    Add theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
-import { Box, Container, Stack, StackDivider } from "@chakra-ui/react"
+import { Text, Container, Stack, StackDivider } from "@chakra-ui/react"
 
 import Nav from "./Nav"
 
 function App() {
   return (
     <Container maxWidth="container.lg">
-      <Stack spacing={0} divider={<StackDivider/>}>
-        <Nav/>
-        <Box marginTop={6}>Contenido</Box>
+      <Stack spacing={0} divider={<StackDivider />}>
+        <Nav />
+        <Stack marginTop={6} direction="row">
+          <Text flex={1}>Parte izquierda</Text>
+          <Stack flex={1}>
+            <Text fontWeight="bold" letterSpacing={2} textTransform="uppercase" fontSize="sm" color="primary.500">Sneaker company</Text>
+            <Text>Parte derecha</Text>
+            <Text>Parte derecha</Text>
+            <Text>Parte derecha</Text>
+            <Text>Parte derecha</Text>
+          </Stack>
+        </Stack>
       </Stack>
     </Container>
   )
